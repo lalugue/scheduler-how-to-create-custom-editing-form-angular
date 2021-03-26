e63ca29172592e5d162bfb99682df97e61167f26
lalugue <51893731+lalugue@users.noreply.github.com>
2021-03-26 18:24:51 +0800

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component, HostBinding } from '@angular/core';
-import notify from "devextreme/ui/notify";
-import { Service, Data } from "./app.service";
-import Query from "devextreme/data/query";
+import notify from 'devextreme/ui/notify';
+import { Service, Data } from './app.service';
 import { DxSchedulerComponent } from 'devextreme-angular';
 import { ViewChild } from '@angular/core';
-import { formatDate } from "devextreme/localization";
+import { formatDate } from 'devextreme/localization';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [Service]
+  providers: [Service],
 })
-export class AppComponent  {
+export class AppComponent {
   @ViewChild(DxSchedulerComponent, { static: false })
   scheduler: DxSchedulerComponent;
 
@@ -19,9 +18,7 @@ export class AppComponent  {
   currentDate: Date = new Date(2015, 4, 25);
   movieInfo: any;
 
-  /**Custom Popup Items**/
   isCustomPopupVisible: boolean = false;
-  //oldAppointmentData: any = {};
   editAppointmentData: any = {};
   rows: any;
   seats: any;
@@ -31,49 +28,50 @@ export class AppComponent  {
 
   constructor(service: Service) {
     this.data = service.getData();
-
     this.rows = service.getRows();
-    this.seats = service.getSeats();    
+    this.seats = service.getSeats();
   }
 
   onAppointmentFormOpening = (e) => {
-    e.cancel = true;    
-    this.editAppointmentData = {...e.appointmentData};
-    //this.oldAppointmentData = {...this.editAppointmentData};
-    console.log(this.editAppointmentData);
+    e.cancel = true;
+    this.editAppointmentData = { ...e.appointmentData };
     this.isCustomPopupVisible = true;
   };
 
-  onHiding = (e) => {    
-    //this.oldAppointmentData = {};
+  onHiding = (e) => {
     this.editAppointmentData = {};
   };
 
-  updateAppointment = (e) => {    
-    let oldAppointmentData = this.data.find(e=>{      
-      return e.id == this.editAppointmentData.id
-    });    
-    this.scheduler.instance.updateAppointment(oldAppointmentData, this.editAppointmentData);
-    notify("Selected seat " + this.editAppointmentData.seatRow + this.editAppointmentData.seatNumber + " for " + this.editAppointmentData.text + ". Enjoy!");
-    this.isCustomPopupVisible = false;    
+  updateAppointment = (e) => {
+    let oldAppointmentData = this.data.find((e) => {
+      return e.id == this.editAppointmentData.id;
+    });
+    this.scheduler.instance.updateAppointment(
+      oldAppointmentData,
+      this.editAppointmentData
+    );
+    notify(
+      'Selected seat ' +
+        this.editAppointmentData.seatRow +
+        this.editAppointmentData.seatNumber +
+        ' for ' +
+        this.editAppointmentData.text +
+        '. Enjoy!'
+    );
+    this.isCustomPopupVisible = false;
   };
 
   cancelSelection = (e) => {
     this.isCustomPopupVisible = false;
   };
 
-  setSeatPrice(basePrice, row){
+  setSeatPrice(basePrice, row) {
     let rowPrice;
-    if(row == 'A')
-      rowPrice = 1;
-    else if(row == 'B')
-      rowPrice = 2;
-    else if(row == 'C')
-      rowPrice = 3;
-    else if(row == 'D')
-      rowPrice = 4;
-    
+    if (row == 'A') rowPrice = 1;
+    else if (row == 'B') rowPrice = 2;
+    else if (row == 'C') rowPrice = 3;
+    else if (row == 'D') rowPrice = 4;
+
     return basePrice * rowPrice;
   }
-
 }
